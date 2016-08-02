dd1b8dc30e2730e8600b75456ec085e1229ab767
Anthony Hollingsworth <anthony.hollingsworth@elateral.com>
2016-08-02 22:48:49 +0100

    Added running of setup to allow test to work without real browser document object

--- spec/inputArea.spec.js
+++ spec/inputArea.spec.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { InputArea } from './../components/inputArea';
+import Setup from './../setup';
 
 describe('input area specs', () =>{
     it('should contain an input and a button', () => {
@@ -10,5 +11,13 @@ describe('input area specs', () =>{
             <input/>,
             <button>Add</button>
         ])).to.equal(true);
+    });
+
+    it('should accept input', () => {
+        const wrapper = mount(<InputArea/>);
+        const input = wrapper.find('input');
+        input.simulate('change', {target: {value: 'Resin'}});
+        expect(wrapper.state('text')).to.equal('Resin');
+        expect(input.prop('value')).to.equal('Resin');
     });
 });
