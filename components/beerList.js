a05bfb2c05f1f3a734013e6458ade0fc1d56113d
Anthony Hollingsworth <anthony.hollingsworth@elateral.com>
2016-08-04 15:02:14 +0100

    Wired up onDeleteItem using index of item

--- components/beerList.js
+++ components/beerList.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
 
 export class BeerList extends Component {
+
+    constructor() {
+        super();
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+
+    handleDeleteClick(index, props) {
+        console.log(index);
+        this.props.onDeleteItem(index);
+    }
+
     render() {
         return this.props.items ? (
             <ul>
                 {this.props.items.map((item, index) => (
-                    <li key={index}>{item}<Button>Delete</Button></li>
+                    <li key={index}>{item}<Button href={"Fred" + index} className="deleteButton" onClick={this.handleDeleteClick.bind(this, index, this.props)}>Delete</Button></li>
                 ))}
             </ul>)
         : null;
@@ -14,5 +25,6 @@ export class BeerList extends Component {
 }
 
 BeerList.PropTypes = {
-    items: React.PropTypes.array.isRequired
+    items: React.PropTypes.array.isRequired,
+    onDeleteItem: React.PropTypes.func.isRequired
 };
--- spec/beerList.spec.js
+++ spec/beerList.spec.js
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { BeerList } from './../components/beerList';
 import Setup from './../setup'
+import { spy } from 'sinon';
 
 describe('beerlist specs', () => {
     it('should render zero items', () => {
@@ -25,5 +26,17 @@ describe('beerlist specs', () => {
         const items = ['Sam Adams', 'Resin', 'Octoberfest'];
         const wrapper = shallow(<BeerList items={items}/>);
         expect(wrapper.find('Button')).to.have.length(3);
+    });
+
+    it ('should call onDelete when delete button clicked', () => {
+        const deleteItemSpy = spy();
+        const items = ['Sam Adams', 'Resin', 'Octoberfest'];
+        const wrapper = shallow(<BeerList items={items} onDeleteItem={deleteItemSpy}/>);
+        const deleteButton = wrapper.find('Button.deleteButton').last();
+        //console.log(deleteButton);
+        deleteButton.simulate('click');
+
+        expect(deleteItemSpy.calledOnce).to.equal(true);
+        expect(deleteItemSpy.calledWith(2)).to.equal(true);
     });
 });
